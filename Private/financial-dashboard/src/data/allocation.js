c32fcdc14919753dd38e5a7f89f17a99be64ee8a
humbleguide <andrew@aidos.nyc>
2020-08-12 15:49:26 -0400

    updated

--- Private/financial-dashboard/src/data/allocation.js
+++
@@ -1,6 +0,0 @@
-export const allocation = [
-  { category: "Equity", value: 64},
-  { category: "Fixed Income", value: 28},
-  { category: "Commodity Related", value: 4},
-  { category: "Cash", value: 4},
-];
